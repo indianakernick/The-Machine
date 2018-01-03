2f8fad689ec659172fa606040b7f922613d0f95a
Kerndog73 <kerndog73@gmail.com>
2018-01-03 13:39:23 +1030

    Setup project

---
+++ The Machine/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  The Machine
+//
+//  Created by Indi Kernick on 3/1/18.
+//  Copyright © 2018 Indi Kernick. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int, char **) {
+  std::cout << "Hello world\n";
+  return 0;
+}
